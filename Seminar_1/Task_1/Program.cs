87c9c9d25b63829c8a6380ca8e1704d36db82268
Светикова Марина <m-golovleva@yandex.ru>
2022-09-05 15:22:07 +0500

    realized task0

---
+++ Seminar_1/Task_1/Program.cs
@@ -0,0 +1,5 @@
+﻿// Программа, которая принимает на вход число и выдает его квадрат
+Console.WriteLine ("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine(number*number);
+
